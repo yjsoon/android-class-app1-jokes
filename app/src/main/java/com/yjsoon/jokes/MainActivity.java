a774da2630a4d2005f38253d83a6707e94412313
YJ Soon <yjsoon@tinkertanker.com>
2015-05-06 18:42:23 +0800

    Finished Ex 1B.4 - Click button to show punchline

--- app/src/main/java/com/yjsoon/jokes/MainActivity.java
+++ app/src/main/java/com/yjsoon/jokes/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -17,11 +18,17 @@ public class MainActivity extends ActionBarActivity {
 
         TextView jokeQuestionText = (TextView) findViewById(R.id.joke_question_text);
         Button showPunchlineButton = (Button) findViewById(R.id.show_punchline_button);
-        TextView punchlineText = (TextView) findViewById(R.id.punchline_text);
+        final TextView punchlineText = (TextView) findViewById(R.id.punchline_text);
 
         jokeQuestionText.setText("Dad, did you get a haircut?");
         punchlineText.setText("No, I got them all cut.");
 
+        showPunchlineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                punchlineText.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
 
